02fcdfe5409231af75fdf4eb412fcbaecfd1f28b
Marcelo Puebla <marcenp23@gmail.com>
2023-08-02 09:20:05 -0300

    feat(player-movement): logic improves
    Moved file player movement to behaviours folder
    Improved movement logic

--- Assets/Scripts/Behaviours/PlayerMovement.cs
+++ Assets/Scripts/Behaviours/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
   [SerializeField] private float moveSpeed = 5f;
   private Rigidbody2D rb;
   private Vector2 moveDirection;
+  private Vector2 lastMoveDirection = Vector2.zero;
 
   private void Awake()
   {
@@ -12,9 +13,13 @@ public class PlayerMovement : MonoBehaviour
     rb.gravityScale = 0f; // Freeze the gravity so that the player doesn't fall
   }
 
-  private void FixedUpdate()
+  private void Update()
   {
     GetInputDirection();
+  }
+
+  private void FixedUpdate()
+  {
     Move();
   }
 
@@ -26,11 +31,21 @@ public class PlayerMovement : MonoBehaviour
     // Use ternary operators to set the move direction
     moveDirection.x = Mathf.Abs(moveHorizontal) > Mathf.Abs(moveVertical) ? moveHorizontal : 0f;
     moveDirection.y = Mathf.Abs(moveVertical) > Mathf.Abs(moveHorizontal) ? moveVertical : 0f;
+
+    // Return if the last input direction it zero
+    if (moveDirection == Vector2.zero) return;
+
+    // Check if the new input is not opposite to the lastMoveDirection
+    if (Vector2.Dot(moveDirection.normalized, lastMoveDirection.normalized) >= 0f)
+    {
+      lastMoveDirection = moveDirection.normalized;
+    }
+
   }
 
   private void Move()
   {
     transform.position = BoundaryBehaviour.Instance.GetClampPosition(transform.position);
-    rb.velocity = moveDirection * moveSpeed * Time.fixedDeltaTime * 100;
+    rb.velocity = lastMoveDirection * moveSpeed * Time.fixedDeltaTime * 100;
   }
 }
