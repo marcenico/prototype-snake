af79f2f42dd79fd0a84a66edfa736733c07b47da
Marcelo Puebla <marcenp23@gmail.com>
2023-08-26 00:19:40 -0300

    refactor(player): improve player movement and logic to grow player size when collect items

--- Assets/Scripts/Behaviours/PlayerBehaviour.cs
+++ Assets/Scripts/Behaviours/PlayerBehaviour.cs
@@ -4,19 +4,31 @@ using System.Collections.Generic;
 public class PlayerBehaviour : MonoBehaviour
 {
   [SerializeField] private float moveSpeed = 5f;
-  [SerializeField] private Transform visual;
-  [SerializeField] private GameObject segmentPrefab; // Prefab for the player segments
+  [SerializeField] private float rotationSpeed = 15f;
+  [SerializeField] private GameObject segmentPrefab;
 
   private Rigidbody2D rb;
-  private Vector2 moveDirection;
-  private Vector2 lastMoveDirection = Vector2.zero;
-  private List<Transform> segments = new List<Transform>(); // Array to hold the player segments
-  private readonly float segmentSpacing = -0.5f; // Spacing between segments
+  private Vector2 moveDirection = Vector2.up;
+  public List<Transform> segments = new();
+  private readonly float segmentSpacing = 0.2f;
+
+  private readonly Dictionary<KeyCode, Vector2> directionMappings = new()
+  {
+    { KeyCode.W, Vector2.up },
+    { KeyCode.S, Vector2.down },
+    { KeyCode.A, Vector2.left },
+    { KeyCode.D, Vector2.right }
+  };
 
   private void Awake()
   {
     rb = GetComponent<Rigidbody2D>();
     rb.gravityScale = 0f;
+  }
+
+  private void Start()
+  {
+    segments.Add(transform);
   }
 
   private void Update()
@@ -31,49 +43,49 @@ public class PlayerBehaviour : MonoBehaviour
 
   private void OnTriggerEnter2D(Collider2D other)
   {
-    if (other.CompareTag("Item"))
-    {
-      IncreasePlayerSize();
-      Destroy(other.gameObject);
-    }
+    if (other.CompareTag("Item")) IncreasePlayerSize();
   }
 
   private void IncreasePlayerSize()
   {
-    int playerSize = segments.Count + 1;
-    Vector3 spawnOffset = lastMoveDirection * segmentSpacing * playerSize;
-    Vector3 spawnPosition = visual.position + spawnOffset;
-
-    Transform newSegment = Instantiate(segmentPrefab, spawnPosition, Quaternion.identity).transform;
-    newSegment.parent = transform;
+    Transform newSegment = Instantiate(segmentPrefab, transform.position, Quaternion.identity).transform;
     segments.Add(newSegment);
   }
 
   private void GetInputDirection()
   {
-    float moveHorizontal = Input.GetAxis("Horizontal");
-    float moveVertical = Input.GetAxis("Vertical");
-
-    moveDirection.x = Mathf.Abs(moveHorizontal) > Mathf.Abs(moveVertical) ? moveHorizontal : 0f;
-    moveDirection.y = Mathf.Abs(moveVertical) > Mathf.Abs(moveHorizontal) ? moveVertical : 0f;
-
-    if (moveDirection == Vector2.zero) return;
-
-    if (Vector2.Dot(moveDirection.normalized, lastMoveDirection.normalized) >= 0f)
+    foreach (var kvp in directionMappings)
     {
-      lastMoveDirection = moveDirection.normalized;
+      if (Input.GetKeyDown(kvp.Key))
+      {
+        moveDirection = kvp.Value;
+        break;
+      }
     }
   }
 
   private void Move()
   {
-    rb.velocity = lastMoveDirection * moveSpeed * Time.fixedDeltaTime * 100;
-
-    if (lastMoveDirection != Vector2.zero)
+    for (int i = segments.Count - 1; i > 0; i--)
     {
-      float angle = Mathf.Atan2(lastMoveDirection.y, lastMoveDirection.x) * Mathf.Rad2Deg;
-      visual.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+      segments[i].position = segments[i - 1].position;
     }
+
+    Vector3 newPosition = transform.position + moveSpeed * Time.fixedDeltaTime * (Vector3)moveDirection;
+
+    rb.MovePosition(newPosition);
+    RotateThroughDirection();
+  }
+
+  private void RotateThroughDirection()
+  {
+    if (moveDirection == Vector2.zero) return;
+
+    float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+    float currentAngle = transform.eulerAngles.z;
+    float newAngle = Mathf.LerpAngle(currentAngle, targetAngle, Time.fixedDeltaTime * rotationSpeed);
+
+    transform.rotation = Quaternion.Euler(0f, 0f, newAngle);
   }
 
 }
