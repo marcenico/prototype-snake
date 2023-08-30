d2c01324bc6af0c990b3739635217f0cb0e74710
Marcelo Puebla <marcenp23@gmail.com>
2023-08-30 20:29:51 -0300

    feat(countdown): added countdown gameobject with script

---
+++ Assets/Scripts/Behaviours/Countdown.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour
+{
+  [SerializeField] private float countdownDuration = 3f; // Duration of the countdown in seconds
+  [SerializeField] private TextMeshProUGUI countdownText; // Reference to the UI text that displays the countdown
+
+  private void Start()
+  {
+    StartCoroutine(StartCountdown());
+  }
+
+  private IEnumerator StartCountdown()
+  {
+    int countdownValue = (int)countdownDuration;
+    countdownText.gameObject.SetActive(true);
+
+    while (countdownValue > 0)
+    {
+      countdownText.text = countdownValue.ToString();
+      yield return new WaitForSeconds(1f);
+      countdownValue--;
+    }
+
+    countdownText.text = "Go!";
+    yield return new WaitForSeconds(1f);
+
+    countdownText.gameObject.SetActive(false);
+
+    // Start your gameplay logic here
+  }
+}
